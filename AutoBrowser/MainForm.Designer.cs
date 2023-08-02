7ab3ac1e57ee127c8beb7873d3b79176499ea71b
moka <qnwnen0228@gmail.com>
2023-08-02 12:48:12 +0900

    1.0.0.2 UI개선

--- AutoBrowser/MainForm.Designer.cs
+++ AutoBrowser/MainForm.Designer.cs
@@ -31,8 +31,6 @@ namespace AutoBrowser
         {
             this.components = new System.ComponentModel.Container();
             this.MenuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.ToolStripMenuItemWorkAdd = new System.Windows.Forms.ToolStripMenuItem();
-            this.ToolStripMenuItemWorkDo = new System.Windows.Forms.ToolStripMenuItem();
             this.ToolStripMenuItemWorkSave = new System.Windows.Forms.ToolStripMenuItem();
             this.ToolStripMenuItemWorkLoad = new System.Windows.Forms.ToolStripMenuItem();
             this.ToolStripMenuItemSetting = new System.Windows.Forms.ToolStripMenuItem();
@@ -46,8 +44,18 @@ namespace AutoBrowser
             this.panel2 = new System.Windows.Forms.Panel();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.chromiumWebBrowser = new CefSharp.WinForms.ChromiumWebBrowser();
+            this.panel4 = new System.Windows.Forms.Panel();
             this.dataGridViewEvents = new System.Windows.Forms.DataGridView();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBoxValue = new System.Windows.Forms.RichTextBox();
+            this.textBoxPath = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxEvent = new System.Windows.Forms.ComboBox();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.buttonWorkAdd = new System.Windows.Forms.Button();
+            this.buttonWorkDo = new System.Windows.Forms.Button();
             this.MenuStrip1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.MainPanel.SuspendLayout();
@@ -56,15 +64,15 @@ namespace AutoBrowser
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
+            this.panel4.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEvents)).BeginInit();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // MenuStrip1
             // 
             this.MenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.MenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.ToolStripMenuItemWorkAdd,
-            this.ToolStripMenuItemWorkDo,
             this.ToolStripMenuItemWorkSave,
             this.ToolStripMenuItemWorkLoad,
             this.ToolStripMenuItemSetting,
@@ -72,23 +80,9 @@ namespace AutoBrowser
             this.MenuStrip1.Location = new System.Drawing.Point(0, 0);
             this.MenuStrip1.Name = "MenuStrip1";
             this.MenuStrip1.Padding = new System.Windows.Forms.Padding(4, 1, 0, 1);
-            this.MenuStrip1.Size = new System.Drawing.Size(1004, 24);
+            this.MenuStrip1.Size = new System.Drawing.Size(1184, 24);
             this.MenuStrip1.TabIndex = 0;
             this.MenuStrip1.Text = "menuStrip1";
-            // 
-            // ToolStripMenuItemWorkAdd
-            // 
-            this.ToolStripMenuItemWorkAdd.Name = "ToolStripMenuItemWorkAdd";
-            this.ToolStripMenuItemWorkAdd.Size = new System.Drawing.Size(67, 22);
-            this.ToolStripMenuItemWorkAdd.Text = "동작추가";
-            this.ToolStripMenuItemWorkAdd.Click += new System.EventHandler(this.ToolStripMenuItemWorkAdd_Click);
-            // 
-            // ToolStripMenuItemWorkDo
-            // 
-            this.ToolStripMenuItemWorkDo.Name = "ToolStripMenuItemWorkDo";
-            this.ToolStripMenuItemWorkDo.Size = new System.Drawing.Size(67, 22);
-            this.ToolStripMenuItemWorkDo.Text = "동작실행";
-            this.ToolStripMenuItemWorkDo.Click += new System.EventHandler(this.ToolStripMenuItemWorkDo_Click);
             // 
             // ToolStripMenuItemWorkSave
             // 
@@ -129,7 +123,7 @@ namespace AutoBrowser
             this.panel1.Controls.Add(this.buttonBack);
             this.panel1.Location = new System.Drawing.Point(0, 24);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1004, 23);
+            this.panel1.Size = new System.Drawing.Size(1184, 23);
             this.panel1.TabIndex = 0;
             // 
             // textBoxUrl
@@ -138,7 +132,7 @@ namespace AutoBrowser
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textBoxUrl.Location = new System.Drawing.Point(201, 1);
             this.textBoxUrl.Name = "textBoxUrl";
-            this.textBoxUrl.Size = new System.Drawing.Size(800, 21);
+            this.textBoxUrl.Size = new System.Drawing.Size(980, 21);
             this.textBoxUrl.TabIndex = 3;
             this.textBoxUrl.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxUrl_KeyDown);
             // 
@@ -179,7 +173,7 @@ namespace AutoBrowser
             this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.MainPanel.Location = new System.Drawing.Point(0, 24);
             this.MainPanel.Name = "MainPanel";
-            this.MainPanel.Size = new System.Drawing.Size(1004, 537);
+            this.MainPanel.Size = new System.Drawing.Size(1184, 737);
             this.MainPanel.TabIndex = 1;
             // 
             // panel2
@@ -190,7 +184,7 @@ namespace AutoBrowser
             this.panel2.Controls.Add(this.splitContainer1);
             this.panel2.Location = new System.Drawing.Point(0, 28);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1004, 510);
+            this.panel2.Size = new System.Drawing.Size(1184, 710);
             this.panel2.TabIndex = 0;
             // 
             // splitContainer1
@@ -205,9 +199,10 @@ namespace AutoBrowser
             // 
             // splitContainer1.Panel2
             // 
-            this.splitContainer1.Panel2.Controls.Add(this.dataGridViewEvents);
-            this.splitContainer1.Size = new System.Drawing.Size(1004, 510);
-            this.splitContainer1.SplitterDistance = 664;
+            this.splitContainer1.Panel2.Controls.Add(this.panel4);
+            this.splitContainer1.Panel2.Controls.Add(this.panel3);
+            this.splitContainer1.Size = new System.Drawing.Size(1184, 710);
+            this.splitContainer1.SplitterDistance = 849;
             this.splitContainer1.TabIndex = 0;
             // 
             // chromiumWebBrowser
@@ -216,8 +211,19 @@ namespace AutoBrowser
             this.chromiumWebBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
             this.chromiumWebBrowser.Location = new System.Drawing.Point(0, 0);
             this.chromiumWebBrowser.Name = "chromiumWebBrowser";
-            this.chromiumWebBrowser.Size = new System.Drawing.Size(664, 510);
+            this.chromiumWebBrowser.Size = new System.Drawing.Size(849, 710);
             this.chromiumWebBrowser.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel4.Controls.Add(this.dataGridViewEvents);
+            this.panel4.Location = new System.Drawing.Point(2, 218);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(329, 489);
+            this.panel4.TabIndex = 1;
             // 
             // dataGridViewEvents
             // 
@@ -229,9 +235,87 @@ namespace AutoBrowser
             this.dataGridViewEvents.Name = "dataGridViewEvents";
             this.dataGridViewEvents.RowHeadersWidth = 62;
             this.dataGridViewEvents.RowTemplate.Height = 23;
-            this.dataGridViewEvents.Size = new System.Drawing.Size(336, 510);
+            this.dataGridViewEvents.Size = new System.Drawing.Size(329, 489);
             this.dataGridViewEvents.TabIndex = 0;
             this.dataGridViewEvents.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewEvents_CellEndEdit);
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel3.Controls.Add(this.buttonWorkDo);
+            this.panel3.Controls.Add(this.buttonWorkAdd);
+            this.panel3.Controls.Add(this.textBoxValue);
+            this.panel3.Controls.Add(this.textBoxPath);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.comboBoxEvent);
+            this.panel3.Location = new System.Drawing.Point(2, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(449, 212);
+            this.panel3.TabIndex = 0;
+            // 
+            // textBoxValue
+            // 
+            this.textBoxValue.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxValue.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBoxValue.Location = new System.Drawing.Point(74, 96);
+            this.textBoxValue.Name = "textBoxValue";
+            this.textBoxValue.Size = new System.Drawing.Size(252, 50);
+            this.textBoxValue.TabIndex = 24;
+            this.textBoxValue.Text = "";
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxPath.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBoxPath.Location = new System.Drawing.Point(74, 40);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(252, 50);
+            this.textBoxPath.TabIndex = 23;
+            this.textBoxPath.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "입력";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 12);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "HTML 경로";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 12);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "동작 선택";
+            // 
+            // comboBoxEvent
+            // 
+            this.comboBoxEvent.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxEvent.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxEvent.FormattingEnabled = true;
+            this.comboBoxEvent.Location = new System.Drawing.Point(74, 3);
+            this.comboBoxEvent.Name = "comboBoxEvent";
+            this.comboBoxEvent.Size = new System.Drawing.Size(252, 20);
+            this.comboBoxEvent.TabIndex = 18;
+            this.comboBoxEvent.SelectedValueChanged += new System.EventHandler(this.comboBoxEvent_SelectedValueChanged);
             // 
             // contextMenuStrip1
             // 
@@ -239,11 +323,35 @@ namespace AutoBrowser
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
+            // buttonWorkAdd
+            // 
+            this.buttonWorkAdd.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonWorkAdd.Location = new System.Drawing.Point(3, 152);
+            this.buttonWorkAdd.Name = "buttonWorkAdd";
+            this.buttonWorkAdd.Size = new System.Drawing.Size(323, 23);
+            this.buttonWorkAdd.TabIndex = 25;
+            this.buttonWorkAdd.Text = "동작 추가";
+            this.buttonWorkAdd.UseVisualStyleBackColor = true;
+            this.buttonWorkAdd.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonWorkDo
+            // 
+            this.buttonWorkDo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonWorkDo.Location = new System.Drawing.Point(3, 181);
+            this.buttonWorkDo.Name = "buttonWorkDo";
+            this.buttonWorkDo.Size = new System.Drawing.Size(323, 23);
+            this.buttonWorkDo.TabIndex = 26;
+            this.buttonWorkDo.Text = "동작 실행";
+            this.buttonWorkDo.UseVisualStyleBackColor = true;
+            this.buttonWorkDo.Click += new System.EventHandler(this.ToolStripMenuItemWorkDo_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1004, 561);
+            this.ClientSize = new System.Drawing.Size(1184, 761);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.MainPanel);
             this.Controls.Add(this.MenuStrip1);
@@ -261,7 +369,10 @@ namespace AutoBrowser
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEvents)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -270,14 +381,12 @@ namespace AutoBrowser
         #endregion
 
         private System.Windows.Forms.MenuStrip MenuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemWorkAdd;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button buttonBack;
         private System.Windows.Forms.Button buttonForward;
         private System.Windows.Forms.Button buttonReload;
         private System.Windows.Forms.TextBox textBoxUrl;
         private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemSetting;
-        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemWorkDo;
         private System.Windows.Forms.Panel MainPanel;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.SplitContainer splitContainer1;
@@ -287,6 +396,16 @@ namespace AutoBrowser
         private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemWorkLoad;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.ToolStripMenuItem devToolsToolStripMenuItem;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.RichTextBox textBoxValue;
+        private System.Windows.Forms.RichTextBox textBoxPath;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxEvent;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button buttonWorkAdd;
+        private System.Windows.Forms.Button buttonWorkDo;
     }
 }
 
--- AutoBrowser/MainForm.cs
+++ AutoBrowser/MainForm.cs
@@ -42,7 +42,94 @@ namespace AutoBrowser
             bindings = new BindingList<WorkEvent>(WorkManager.WorkEvents);
             this.dataGridViewEvents.DataSource = bindings;
             this.dataGridViewEvents.Columns[0].ReadOnly = true;
+
+            this.comboBoxEvent.DataSource = Enum.GetValues(typeof(BrowserEvent));
+
         }
+
+        private void comboBoxEvent_SelectedValueChanged(object sender, EventArgs e)
+        {
+
+            ComboBox combox = sender as ComboBox;
+            int select = combox.SelectedIndex;
+            switch (select)
+            {
+                case 0:
+                    this.textBoxPath.Enabled = true;
+                    this.textBoxValue.Enabled = false;
+                    break;
+                case 2:
+                    this.textBoxPath.Enabled = false;
+                    this.textBoxValue.Enabled = true;
+                    break;
+                case 3:
+                    this.textBoxPath.Enabled = true;
+                    this.textBoxValue.Enabled = false;
+                    break;
+                case 1:
+                case 4:
+                default:
+                    this.textBoxPath.Enabled = true;
+                    this.textBoxValue.Enabled = true;
+                    break;
+            }
+            this.textBoxPath.Text = "";
+            this.textBoxValue.Text = "";
+        }
+
+        private void textBoxValue_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Back) return;
+            if (this.comboBoxEvent.SelectedIndex == 2)
+            {
+                var tb = sender as RichTextBox;
+                if (System.Text.RegularExpressions.Regex.IsMatch(tb.Text, "[^0-9]"))
+                {
+                    MessageBox.Show("대기는 숫자만 입력이 가능합니다");
+                    tb.Text = "";
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                BrowserEvent _event = (BrowserEvent)Enum.Parse(typeof(BrowserEvent), comboBoxEvent.SelectedIndex.ToString());
+
+                switch (_event)
+                {
+                    case BrowserEvent.Click:
+                        if (string.IsNullOrWhiteSpace(textBoxPath.Text)) throw new Exception("위치정보가 필요합니다");
+                        break;
+                    case BrowserEvent.Input:
+                        if (string.IsNullOrWhiteSpace(textBoxPath.Text)) throw new Exception("위치정보가 필요합니다");
+                        if (string.IsNullOrWhiteSpace(textBoxValue.Text)) throw new Exception("입력 값이 필요합니다");
+                        break;
+                    case BrowserEvent.Wait:
+                        if (string.IsNullOrWhiteSpace(textBoxValue.Text)) throw new Exception("입력 값이 필요합니다");
+                        break;
+                    case BrowserEvent.Load:
+                    case BrowserEvent.Get:
+                        if (string.IsNullOrWhiteSpace(textBoxPath.Text)) throw new Exception("위치정보가 필요합니다");
+                        break;
+                    default:
+                        break;
+                }
+
+                var workEvent = new WorkEvent(_event, textBoxPath.Text, textBoxValue.Text);
+                WorkManager.WorkEvents.Add(workEvent);
+                this.textBoxPath.Text = "";
+                this.textBoxValue.Text = "";
+                DataReload();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+
 
         private async void ToolStripMenuItemWorkDo_Click(object sender, EventArgs e)
         {
